ba2430fe83d692a1624e7e76e781ef7b28b8998c
soham surve <69713742+Sohamsurve@users.noreply.github.com>
2023-08-14 15:41:00 +0530

    first commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,13 +16,7 @@ const Home = () => {
    <AnimatePresence>
     {snap.intro && (
         <motion.section className='home' {...slideAnimation('left')}>
-            <motion.header {...slideAnimation('down')}>
-               <img 
-                src='./threejs.png'
-                alt='logo'
-                className='w-8 h-8 object-contain'
-               />
-            </motion.header>
+            
             <motion.div className='home-content' {...headContainerAnimation}>
                 <motion.div {...headTextAnimation}>
                     <h1 className='head-text '>
